8d91fa35fc9234f3af690d78821892186556106a
paultaki <paultaki@gmail.com>
2025-01-11 00:21:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
     const loadouts = {
         Light: {
-            weapons: ["93R", "Dagger", "LHI", "M26 Matter", "Recurve Bow", "Sword", "V9S", "XP-54"],
+            weapons: ["93R", "Dagger", "LH1", "M26 Matter", "Recurve Bow", "Sword", "V9S", "XP-54"],
             specializations: ["Cloaking Device", "Evasive Dash", "Grappling Hook"],
             gadgets: ["Breach Charge", "Gateway", "Glitch Grenade", "Gravity Vortex", "Sonar Grenade", "Stun Gun", "Thermal Bore", "Thermal Vision", "Tracking Dart", "Vanishing Bomb"]
         },
