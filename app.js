8e1218dad6c146b0861734708182c61e9ad4a640
Paul Takisaki <paultaki@gmail.com>
2025-01-10 21:29:40 -0700

    Updated with image integration and faster animations

--- app.js
+++ app.js
@@ -1,66 +1,155 @@
 document.addEventListener("DOMContentLoaded", () => {
   const outputDiv = document.getElementById("output");
 
-  const loadoutTypes = {
+  const loadouts = {
       light: {
-          specialization: "Cloaking Device",
-          weapon: "M11",
-          gadgets: ["Smoke Grenade", "Sonar Grenade", "Gateway"],
+          specializations: ["Cloaking Device", "Evasive Dash", "Grappling Hook"],
+          weapons: ["93R", "LH1", "Recurve Bow", "Sword", "Throwing Knives"],
+          gadgets: [
+              "Breach Charge",
+              "Gateway",
+              "Glitch Grenade",
+              "Gravity Vortex",
+              "Sonar Grenade",
+              "Stun Gun",
+              "Thermal Vision",
+              "Tracking Dart",
+              "Vanishing Bomb",
+          ],
       },
       medium: {
-          specialization: "Guardian Turret",
-          weapon: "AKM",
-          gadgets: ["Frag Grenade", "Gas Mine", "Jump Pad"],
+          specializations: ["Guardian Turret", "Healing Beam", "Dematerializer"],
+          weapons: [
+              "AKM",
+              "Cerberus 12GA",
+              "FAMAS",
+              "FCAR",
+              "Model 1887",
+              "R357",
+          ],
+          gadgets: [
+              "APS Turret",
+              "Data Reshaper",
+              "Defibrillator",
+              "Explosive Mine",
+              "Gas Mine",
+              "Glitch Trap",
+              "Jump Pad",
+              "Proximity Sensor",
+              "Zipline",
+          ],
       },
       heavy: {
-          specialization: "Mesh Shield",
-          weapon: "ShAK-50",
-          gadgets: ["Pyro Grenade", "Lockbolt Launcher", "Dome Shield"],
+          specializations: ["Charge 'N' Slam", "Goo Gun", "Mesh Shield"],
+          weapons: [
+              "50 Akimbo",
+              "Flamethrower",
+              "KS-23",
+              "Lewis Gun",
+              "M60",
+              "ShAK-50",
+              "Sledgehammer",
+          ],
+          gadgets: [
+              "Anti-Gravity Cube",
+              "Barricade",
+              "C4",
+              "Dome Shield",
+              "Lockbolt Launcher",
+              "Pyro Grenade",
+              "Pyro Mine",
+              "RPG-7",
+          ],
       },
+      universalGadgets: [
+          "Flashbang",
+          "Frag Grenade",
+          "Gas Grenade",
+          "Goo Grenade",
+          "Smoke Grenade",
+      ],
   };
 
-  function createLoadout(loadout) {
+  function getRandomItem(array) {
+      return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function getUniqueGadgets(gadgetPool, count) {
+      const uniqueGadgets = [];
+      while (uniqueGadgets.length < count) {
+          const gadget = getRandomItem(gadgetPool);
+          if (!uniqueGadgets.includes(gadget)) {
+              uniqueGadgets.push(gadget);
+          }
+      }
+      return uniqueGadgets;
+  }
+
+  function generateLoadout(classType) {
+      const classData = loadouts[classType];
+      const randomSpecialization = getRandomItem(classData.specializations);
+      const randomWeapon = getRandomItem(classData.weapons);
+      const randomGadgets = getUniqueGadgets(
+          [...classData.gadgets, ...loadouts.universalGadgets],
+          3
+      );
+
       outputDiv.innerHTML = `
-          <div>
-              <img src="images/${loadout.specialization.replaceAll(" ", "_")}_Rank_1.png" alt="${loadout.specialization}">
-              <p>${loadout.specialization.toUpperCase()}</p>
-          </div>
-          <div>
-              <img src="images/${loadout.weapon.replaceAll(" ", "_")}_Rank_1.png" alt="${loadout.weapon}">
-              <p>${loadout.weapon.toUpperCase()}</p>
-          </div>
-          <div class="gadget-row">
-              ${loadout.gadgets
-                  .map(
-                      (gadget) => `
-                  <div>
-                      <img src="images/${gadget.replaceAll(" ", "_")}_Rank_1.png" alt="${gadget}">
-                      <p>${gadget.toUpperCase()}</p>
+          <div class="loadout">
+              <div class="loadout-item">
+                  <h3>Specialization:</h3>
+                  <img src="images/${randomSpecialization.replaceAll(
+                      " ",
+                      "_"
+                  )}_Rank_1.png" alt="${randomSpecialization}">
+                  <p>${randomSpecialization}</p>
+              </div>
+              <div class="loadout-item">
+                  <h3>Weapon:</h3>
+                  <img src="images/${randomWeapon.replaceAll(
+                      " ",
+                      "_"
+                  )}_Rank_1.png" alt="${randomWeapon}">
+                  <p>${randomWeapon}</p>
+              </div>
+              <div class="loadout-item">
+                  <h3>Gadgets:</h3>
+                  <div class="gadget-row">
+                      ${randomGadgets
+                          .map(
+                              (gadget) => `
+                          <div>
+                              <img src="images/${gadget.replaceAll(
+                                  " ",
+                                  "_"
+                              )}_Rank_1.png" alt="${gadget}">
+                              <p>${gadget}</p>
+                          </div>
+                      `
+                          )
+                          .join("")}
                   </div>
-              `
-                  )
-                  .join("")}
+              </div>
           </div>
       `;
   }
 
+  // Event Listeners
   document.getElementById("randomizeClass").onclick = () => {
-      const randomClass =
-          Object.keys(loadoutTypes)[
-              Math.floor(Math.random() * Object.keys(loadoutTypes).length)
-          ];
-      createLoadout(loadoutTypes[randomClass]);
+      const classTypes = ["light", "medium", "heavy"];
+      const randomClass = getRandomItem(classTypes);
+      generateLoadout(randomClass);
   };
 
   document.getElementById("lightButton").onclick = () => {
-      createLoadout(loadoutTypes.light);
+      generateLoadout("light");
   };
 
   document.getElementById("mediumButton").onclick = () => {
-      createLoadout(loadoutTypes.medium);
+      generateLoadout("medium");
   };
 
   document.getElementById("heavyButton").onclick = () => {
-      createLoadout(loadoutTypes.heavy);
+      generateLoadout("heavy");
   };
 });
